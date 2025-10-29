c23f406bb95e247ed477081b5dbd8f6c6492478b
rexdavid06 <rhexmilia06@gmail.com>
2025-10-29 22:34:25 +0100

    installed pillow

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://blogapi-i4jr.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
